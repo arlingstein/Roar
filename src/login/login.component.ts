5eba6bef4f8f91b2ee7cbb68a8365d999eb0182c
arlingstein <arlingtonhammy@outlook.com>
2017-10-22 21:11:38 -0700

    intial attempt at capturing video

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from './auth/auth.service';
-import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,35 +10,56 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginform: FormGroup;
-  loginError: boolean;
-  errorMessage: string;
+  mediaOptions = {
+    audio: true,
+    video: { width: 1280, height: 720 },
+    facingMode: "user"
+  }; //delete
 
-  constructor(private fb: FormBuilder,
-              private authService: AuthService,
-              private router: Router) {
-      this.loginError = false;
-      this.loginform = fb.group({
-        'email': ['', Validators.required],
-        'password': ['', Validators.required]
-      });
-   }
 
-  ngOnInit() {
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginform = fb.group({
+      "email": ["", Validators.required],
+      "password": ["", Validators.required]
+    })
   }
 
- loginWithGoogle() {
-   this.authService.loginWithGoogle()
-   .then( res => this.router.navigateByUrl('/home'))
-   .catch(err => console.log(err));
- }
+  ngOnInit() { }
 
-  onSubmit() {
-    const email = this.loginform.get('email').value;
-    const password = this.loginform.get('password').value;
-    console.log(email);
-    console.log(password);
-    this.authService.loginWithEmailAndPassword(email, password)
-     .then(suc => this.router.navigateByUrl('/home'))
-     .catch(err => this.errorMessage = err);
+  onSubmit() { }
+
+  loginWithGoogle() {
+    this.authService.loginWithGoogle();
+  }
+
+  facialRecognition() {
+    console.log("In Facial Recognition");
+    if (!navigator.mediaDevices.getUserMedia) {
+      return alert('getUserMedia not supported in this browser.');
+    }
+    navigator.mediaDevices.getUserMedia(this.mediaOptions)
+      .then(function (mediaStream) {
+        console.log("Promise Successful");
+        var video = document.querySelector('video');
+        debugger;
+        // var video = document.querySelector('');
+        // Older browsers may not have srcObject
+        debugger;
+        if (video.hasAttribute("srcObject")) {
+          video.srcObject = mediaStream;
+        } else {
+          // Avoid using this in new browsers, as it is going away.
+          video.src = window.URL.createObjectURL(mediaStream);
+        }
+        video.onloadedmetadata = function (e) {
+          video.play();
+        };
+      })
+      .catch(function (err) { console.log(err.name + ": " + err.message); }); // alw;
+  }
+
+  success(stream) {
+    window.URL.createObjectURL(stream);
   }
 }
